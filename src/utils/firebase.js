f4c3162b9e2069690464cf93968161c97a875826
Lingamurthy M P <lingamurthy5364@gmail.com>
2024-01-27 15:41:13 +0530

    newcommit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASECONFIGAPTKEY,
+  apiKey: "AIzaSyAbsL6ANfK5segsIPmt6Gp00Po_im49vRA",
   authDomain: "netflixgpt-36586.firebaseapp.com",
   projectId: "netflixgpt-36586",
   storageBucket: "netflixgpt-36586.appspot.com",
